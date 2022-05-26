7bd78a7cb25fbe6b01391aee8aada052f06df1db
gitHUB <gitHUB>
2022-05-26 14:20:25 +0530

    Nithya

---
+++ src/org/test/Sample.java
@@ -0,0 +1,26 @@
+package org.test;
+
+public class Sample {
+	
+	private void test1() {
+		System.out.println("Test 1");
+
+	}
+	
+	private void test2() {
+		System.out.println("Test 2");
+
+	}
+	
+	private void test3() {
+		System.out.println("Test 3");
+
+	}
+	private void test4() {
+		System.out.println("Test 4");
+
+	}
+
+	
+
+}
